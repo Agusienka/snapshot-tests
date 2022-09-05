a85db5b7b39966cef2fcb55182af0658c12dfd5a
Agusienka <101678267+Agusienka@users.noreply.github.com>
2022-09-05 11:24:27 -0500

    done

--- src/GitHubCard.test.js
+++ src/GitHubCard.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, CardImg } from '@testing-library/react';
 import App from './App';
 import renderer from 'react-test-renderer'
 import GitHubCard from './GitHubCard';
@@ -8,3 +8,5 @@ test('renders a snapshot', () => {
     const tree = renderer.create(<GitHubCard/>).toJSON()
     expect(tree).toMatchSnapshot()
   })
+
+  
